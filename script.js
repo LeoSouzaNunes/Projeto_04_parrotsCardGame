e79eab1c257bc5564f76065490fed0a97578ca8b
Leonardo Nunes <contato.leonunez@gmail.com>
2021-11-05 11:21:14 -0300

    Requisito Distribuição de cartas Finalizado

--- script.js
+++ script.js
@@ -1,21 +1,54 @@
 let numeroCartas;
-let contador = 0;
 
 
 function addCards() {
+
     while (!(numeroCartas <= 14 && numeroCartas >= 4 && numeroCartas % 2 === 0)) {
         numeroCartas = parseInt(prompt("Insira um número de 4 até 14 cartas para jogar."));
     }
 
-    let cartas = document.querySelector(".cards");
+    let images = [
+        'images/1.gif',
+        'images/2.gif',
+        'images/3.gif',
+        'images/4.gif',
+        'images/5.gif',
+        'images/6.gif',
+        'images/7.gif',
+    ]
 
-    while (contador < numeroCartas) {
-        cartas.innerHTML = cartas.innerHTML + `   <div class="card" data-identifier="card">
-    <div class="front" data-identifier="front-face"></div>
-    <div class="back" data-identifier="back-face"></div>
-    </div>`
-        contador = contador + 1;
+    images.sort(comparador)
+
+    let pares = [];
+
+    for (let i = 0; i < (numeroCartas / 2); i++) {
+        pares.push(images[i], images[i]);
     }
+
+    pares.sort(comparador)
+
+    let cards = document.querySelector(".cards");
+
+    for (let i = 0; i < numeroCartas; i++) {
+        cards.innerHTML += `   <div onclick="flipCard(this)" class="card" data-identifier="card">
+        <div class="front-face card-layout hidden" data-identifier="front-face">
+        <img src="${pares[i]}" alt="Imagem não encontrada."></img>
+        </div>
+        <div class="back-face card-layout" data-identifier="back-face"></div>
+        </div>`
+    }
+}
+function flipCard(cartaClicada) {
+    let cardFront = cartaClicada.querySelector(".card .front-face");
+    let cardBack = cartaClicada.querySelector(".card .back-face");
+    let cards = document.querySelectorAll(".cards ")
+
+    cardFront.classList.toggle("hidden");
+    cardBack.classList.toggle("hidden");
 }
 
 addCards();
+
+function comparador() {
+    return Math.random() - 0.5;
+}
